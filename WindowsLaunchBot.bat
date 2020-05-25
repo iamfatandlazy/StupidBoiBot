2dc4b23cb1f21687922f6ccc1e06f36a9a2db122
David <ddude2410@live.com>
2020-05-25 03:32:21 -0400

    New launching files, corrected inconsistency in json writting

---
+++ WindowsLaunchBot.bat
@@ -0,0 +1,8 @@
+@echo off
+SETLOCAL
+
+echo ####################https://github.com/iamfatandlazy/StupidBoiBot####################
+echo _____________________________________________________________________________________
+python lib/StupidBoiV2.py
+pause
+exit
